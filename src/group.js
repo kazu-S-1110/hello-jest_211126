5f204bcfd82afcf1356066bf1e5c88fdb7f11593
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-11-27 22:04:03 +0900

    testのスキップ方法を学習。

--- src/group.js
+++ src/group.js
@@ -1,1 +1,19 @@
 export const isSour = (beverage) => (beverage === 'lemonade' ? true : false);
+
+export const getBeverage = (age) => {
+  const beverages = [
+    { name: 'cola', alcohol: false },
+    { name: 'lemonade', alcohol: false },
+    { name: 'beer', alcohol: true },
+  ];
+  const filteredBeverages =
+    age >= 20 ? beverages : beverages.filter((b) => b.alcohol === false);
+
+  const seed = Math.floor(Math.random() * Math.floor(filteredBeverages.length));
+  return filteredBeverages[seed];
+};
+
+console.log(getBeverage(20));
+
+export const fetchData = () =>
+  new Promise((resolve) => setTimeout(resolve, 1000, 'lemon'));
--- src/group.test.js
+++ src/group.test.js
@@ -1,4 +1,4 @@
-import { isSour } from './group';
+import { getBeverage, isSour, fetchData } from './group';
 
 describe('#isSour', () => {
   test('lemonade is sour', () => {
@@ -10,3 +10,38 @@ describe('#isSour', () => {
 });
 
 // 個別のテストを実行するにはyarn test filename
+
+describe('#getBeverage', () => {
+  // Math.random関数をモック化する
+  const spy = jest
+    .spyOn(Math, 'random')
+    .mockImplementationOnce(() => 0.7)
+    .mockImplementationOnce(() => 0);
+  const adult = 20;
+
+  test.skip('return beer when age is 20', () => {
+    expect(getBeverage(adult).name).toBe('beer');
+  });
+
+  it.skip('return cola when age is 20', () => {
+    expect(getBeverage(adult).name).toBe('cola');
+  });
+});
+
+// 全てのテストが終了してから実行される
+afterAll(() => {
+  spy.mockRestore();
+});
+
+const runMultipleTests = (n) => {
+  Array.from(new Array(n).keys()).map((i) => {
+    it.concurrent(`test-${i}:return lemon`, async () => {
+      await expect(fetchData()).resolves.toBe('lemon');
+    });
+  });
+};
+
+// describe.skip, xdescribeってするとスキップされる
+describe.skip('parallel runMultipleTests', () => {
+  runMultipleTests(100);
+});
