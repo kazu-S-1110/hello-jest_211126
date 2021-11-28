f7ee525f09b216286455dcd20d9dfa9bbfdf301f
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-11-28 09:27:14 +0900

    Throwを細かく評価する方法を学習。

--- src/assertion.test.js
+++ src/assertion.test.js
@@ -37,4 +37,8 @@ test('throw Error when passing no variable', () => {
   expect(() => {
     new Foo();
   }).toThrow();
+  expect(() => new Foo()).toThrow(TypeError);
+  expect(() => new Foo()).toThrow(
+    "Cannot destructure property 'message' of 'undefined' as it is undefined."
+  );
 });
